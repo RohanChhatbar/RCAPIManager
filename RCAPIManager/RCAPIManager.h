00a17f74ed9cea30542d6bf93993756285d2d165
Rohan Chhatbar <rohan.chhatbar@ecosmob.com>
2021-06-01 18:11:50 +0530

    Initial Commit

---
+++ RCAPIManager/RCAPIManager.h
@@ -0,0 +1,18 @@
+//
+//  RCAPIManager.h
+//  RCAPIManager
+//
+//  Created by Rohan on 01/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RCAPIManager.
+FOUNDATION_EXPORT double RCAPIManagerVersionNumber;
+
+//! Project version string for RCAPIManager.
+FOUNDATION_EXPORT const unsigned char RCAPIManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCAPIManager/PublicHeader.h>
+
+
